0f8499f1c369f1b0ccdc16e7f4a7205efd99538b
Uzair Tariq <uzair.t2006@gmail.com>
2024-07-10 14:35:41 +0500

    Banner complete Update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,11 @@
 export const Banner = () => {
   return (
-    <div className="py-3 text-center">
+    <div className="py-3 text-center bg-[linear-gradient(to_right,#FCD6FF,#29D8FF,#FFFD80,#F89ABF,#FCD6FF)]">
       <div className="container">
-        <a href="#" className="underline underline-offset-4 font-medium">Explore the demo</a>
+        <p className="font-medium">
+          <span className="hidden sm:inline">Introducing a completely redesigned interface - </span><a href="#" className="underline underline-offset-4">Explore the demo</a>
+          </p>
+       
       </div>
     </div>
   )
